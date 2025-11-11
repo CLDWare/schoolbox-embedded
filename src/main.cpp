770d2215c8f0d918987bb3c344ca6eb65d6e7c6e
Jurn Wubben <jsw@jsw.tf>
2025-11-11 21:08:08 +0100

    Added basic wifi connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,44 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <WiFiMulti.h>
+#include "secrets.h"
 
+
+WiFiMulti wifiMulti;
+
+void (*reset)(void) = 0;
 
 void setup() {
   Serial.begin(115200);
   delay(100);
 
   Serial.println("[BOOT] Serial up!");
+  wifiMulti.addAP(WIFI_SSID, WIFI_PASSWD);
+
+  Serial.println("[BOOT] Connecting to wifi...");
+  for (int i = 10; i > 1; i--) {
+    if (wifiMulti.run() == WL_CONNECTED) {
+      Serial.println("[BOOT] Connected to wifi!");
+      break;
+    }
+
+    Serial.print(".");
+    delay(1000);
+  }
+
+  if (wifiMulti.run() != WL_CONNECTED) {
+    Serial.println("[BOOT] Failed to connect to wifi.");
+    reset();
+    return; // for readability, reset stops the program anyway.
+  }
 }
 
 void loop() {
+  if (wifiMulti.run() != WL_CONNECTED) {
+    Serial.println("[WIFI] Wifi disconnected, restarting.");
+    reset();
+    return;
+  }
+
+
 }
