e451513494397ff4e784126eced8cd62d96e8cde
Jurn Wubben <jsw@jsw.tf>
2025-11-13 21:59:48 +0100

    Tried to add buttons for voting and failed horribly to debounce so now it doesn't work at all.

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,10 @@
 WiFiMulti wifiMulti;
 WebSocket ws = WebSocket(WS_ADDR, WS_PORT, WS_PATH);
 
+const size_t buttonAmount = sizeof(VOTE_BUTTONS) / sizeof(VOTE_BUTTONS[0]);
+bool buttonPressed[buttonAmount] = {false};
+unsigned long buttonDebounce[buttonAmount];
+
 void (*reset)(void) = 0;
 
 void setup() {
@@ -19,6 +23,13 @@ void setup() {
     delay(100);
   }
   Serial.println("[BOOT] Serial up!");
+
+  Serial.println("[BOOT] Setting up buttons.");
+  for (int button : VOTE_BUTTONS) {
+    pinMode(button, INPUT_PULLUP);
+
+    // Serial.printf("[BOOT] button %d is %d.\n", button, digitalRead(button));
+  }
 
   Serial.println("[BOOT] Connecting to wifi...");
   wifiMulti.addAP(WIFI_SSID, WIFI_PASSWD);
@@ -51,4 +62,26 @@ void loop() {
   }
 
   ws.loop();
+
+  for (int i = 0; i < buttonAmount; i++) {
+    unsigned long now = millis();
+    int pin = VOTE_BUTTONS[i];
+    bool value = !digitalRead(pin);
+
+    if (value != buttonPressed[i]) {
+      buttonDebounce[i] = now;
+    }
+
+    if ((now - buttonDebounce[i]) >= 500) {
+      if (value && !buttonPressed[i]) {
+        buttonPressed[i] = true;
+        Serial.printf("[VOTE] %d\n", i);
+        ws.vote(i + 1);
+      } else if (!value && buttonPressed[i]) {
+        buttonPressed[i] = false;
+      }
+    } else {
+      buttonPressed[i] = value;
+    };
+  }
 }
--- src/websocket.cpp
+++ src/websocket.cpp
@@ -131,13 +131,8 @@ void WebSocket::wsHandler(WStype_t type, uint8_t *payload, size_t length) {
 
     break;
 
-  case WStype_BIN:
-  case WStype_ERROR:
-  case WStype_FRAGMENT_TEXT_START:
-  case WStype_FRAGMENT_BIN_START:
-  case WStype_FRAGMENT:
-  case WStype_FRAGMENT_FIN:
-    break;
+    default:
+      break;
   }
 }
 
@@ -146,8 +141,6 @@ void WebSocket::messageHandler(JsonDocument json) {
 
   if (!strcmp(cmd, "ping")) {
     this->ws.sendTXT("{\"c\": \"pong\"}");
-
-    Serial.println("ping ping");
   } else if (!strcmp(cmd, "reg_pin")) {
     if (this->state != WSState::REGISTRATING) {
       Serial.println("[WSc] Received websocket registration command, while I'm "
